56cd5154bcd84e97d1f366446b817d51028f31f6
NagaJahnaviD <nagajahnavidannayak@gmail.com>
2025-04-23 16:08:31 +0530

    Fibonacci series

--- 23071A05N7-2/src/Components/Home.jsx
+++ 23071A05N7-2/src/Components/Home.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import { useState } from 'react'
-import { useForm } from 'react-hook-form';
+import React from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 function RandomStuff() {
-  const [fibnos, setfibnos]=useState([]);
-  const {handleSubmit,register,formState:{errors}}=useForm();
+  const [fibnos, setfibnos] = useState([]);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
   function fib(data) {
     const n = parseInt(data.num);
     let result = [];
     if (n >= 1) result.push(0);
     if (n >= 2) result.push(1);
-    let v1 = 0, v2 = 1;
+    let v1 = 0,
+      v2 = 1;
     for (let i = 2; i < n; i++) {
       const v3 = v1 + v2;
       result.push(v3);
@@ -19,27 +24,38 @@ function RandomStuff() {
     setfibnos(result);
   }
   return (
-    <div className='container'>
-         <form className='text-center form' action="" onClick={handleSubmit((data)=>fib(data))}>
-            <div>
-              <label htmlFor="num">Enter a number</label>
-              <input type="integer " className='form-input' {...register('num',{required:true,min:1})} />
-              {errors.num?.type === 'required'&&<p className='text-warning'>* This is a required field</p>}
-              {errors.num?.type === 'min'&&<p className='text-warning'>* number must be graeter than 0</p>}
-            </div>
-            <button className="btn btn-dark">generate</button>
-         </form>
-         <div className="mt-4">
-            {fibnos.length > 0 && (
-              <div>
-                <h4>Fibonacci Sequence:</h4>
-                <p>{fibnos.join(', ')}</p>
-              </div>
-            )}
-         </div>
-         
+    <div className="container pt-5">
+      <form
+        className="text-center form"
+        action=""
+        onClick={handleSubmit((data) => fib(data))}
+      >
+        <div>
+          <label htmlFor="num">Enter a number</label>
+          <input
+            type="integer "
+            className="form-input"
+            {...register("num", { required: true, min: 1 })}
+          />
+          {errors.num?.type === "required" && (
+            <p className="text-warning">* This is a required field</p>
+          )}
+          {errors.num?.type === "min" && (
+            <p className="text-warning">* number must be graeter than 0</p>
+          )}
+        </div>
+        <button className="btn btn-dark mt-3">generate</button>
+      </form>
+      <div className="mt-4">
+        {fibnos.length > 0 && (
+          <div>
+            <h4>Fibonacci Sequence:</h4>
+            <p>{fibnos.join(", ")}</p>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
-export default RandomStuff
+export default RandomStuff;
